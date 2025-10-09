a9c461a86f46a8ae15aa13e3af5b5c2fa192f8d0
Huy <hoangg.huyy2003@gmail.com>
2025-10-09 12:58:19 +0700

    day 3

--- Plaindrome Number/1.py
+++ Plaindrome Number/1.py
@@ -1,2 +1,28 @@
-a =123
-b=len(a)
+class Solution:
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        output = ""
+        strs = sorted(strs)
+        first = strs[0]
+        last = strs[-1]
+        for i in range(min(len(first), len(last))):
+            if first[i] != last[i]:
+                return output
+            output+= first[i]
+        return output
+    def longestCommonPrefix2(self, strs: list[str]) -> str:
+        output = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return output
+            output += strs[0][i]
+        return output
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.longestCommonPrefix2(["flower","flow","flight"]))
+    print(s.longestCommonPrefix2(["dog","racecar","car"]))
+    print(s.longestCommonPrefix2([""]))
