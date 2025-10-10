a0eea78836805815f5e85180fd482f88ebf1ec98
Huy <hoangg.huyy2003@gmail.com>
2025-10-10 21:49:48 +0700

    Day 4

---
+++ Valid Parenthese/Solution.py
@@ -0,0 +1,56 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open = ["(","[","{"]
+        close = [")","]","}"]
+        dict = {
+            ")": "(",
+            "}": "{",
+            "]": "[",
+        }
+        stack = []
+        list1 =[]
+        list2 =[]
+        for i in range(len(s)):
+            if s[i] in open:
+                list1.append(s[i])
+            if s[i] in close:
+                list2.append(s[i])
+        if len(list1) != len(list2): return False
+        if len(s) % 2 == 1: return False
+        if s[0] in close or (s[0] in open and s[len(s)-1] in open): return False
+        for i in range(len(s)):
+            if s[i] in open:
+                stack.append(s[i])
+            if s[i] in close:
+                if dict[s[i]] != stack.pop():
+                    return False
+        if len(stack) == 0: return True
+
+    def isValid2(self, s: str) -> bool:
+        stack = []
+        mapping = {")": "(", "}": "{", "]": "["}
+
+        for char in s:
+            if char in mapping.values():
+                stack.append(char)
+            elif char in mapping.keys():
+                if not stack or mapping[char] != stack.pop():
+                    return False
+
+        return not stack
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    s = Solution()
+    print(s.isValid2("()[]{}"))
+    print(s.isValid2("(]"))
+    print(s.isValid2("([)]"))
+    print(s.isValid2("(){}}{"))
+    print(s.isValid2("{}{}{}{}{}{(([[]]))]"))
