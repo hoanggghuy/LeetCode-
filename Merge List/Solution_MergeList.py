9f1923dddddfbdd16f7f8cf46c4a3b9de322cdc8
Huy <hoangg.huyy2003@gmail.com>
2025-10-11 14:19:42 +0700

    Day 5 with Linklist

---
+++ Merge List/Solution_MergeList.py
@@ -0,0 +1,22 @@
+from typing import Optional
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) :
+        output = ListNode()
+        cur_node = ouput
+        while list1 and list2:
+            if list1.val > list2.val:
+                cur_node.next =list2
+                list2 = list2.next
+            else:
+                cur_node.next =list1
+                list1 = list1.next
+            cur_node = cur_node.next
+        if list1:
+            cur_node.next = list1
+        if list2:
+            cur_node.next = list2
+        return output.next
