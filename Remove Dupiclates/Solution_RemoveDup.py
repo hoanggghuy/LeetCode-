8b8ae57778924c7d5bd75513ca7ffbcc5d6fd458
Huy <hoangg.huyy2003@gmail.com>
2025-10-13 16:12:17 +0700

    Day7

---
+++ Remove Dupiclates/Solution_RemoveDup.py
@@ -0,0 +1,34 @@
+class Solution:
+    def removeDuplicates(self, nums: list[int]) :
+        k = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i] != nums[j] and nums[i]<= nums[j]:
+                    nums[i+1] = nums[j]
+                    break
+                elif nums[i] == nums[j] :
+                    continue
+            k += 1
+            if nums[i] == nums[-1]:
+                break
+        return nums,k
+    def removeDuplicates2(self, nums: list[int]) :
+        k=1
+        l=0
+        r=1
+        while l <len(nums) and r < len(nums) :
+            if nums[l] < nums[r]:
+                nums[l+1] = nums[r]
+                l+=1
+                r+=1
+                k+=1
+            else:
+                r +=1
+        return nums,k
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+    print(s.removeDuplicates2([1,1,2]))
+    print(s.removeDuplicates2([0,0,1,1,1,2,2,3,3,4]))
