8888ebe55fd69623fb93f7a1d05a31e7b405806d
Huy <hoangg.huyy2003@gmail.com>
2025-10-16 21:30:32 +0700

    Day 10

---
+++ SearchInsertPos/Solution.py
@@ -0,0 +1,22 @@
+class Solution:
+    def searchInsert(self, nums: list[int], target: int) -> int:
+        left = 0
+        right = len(nums) - 1
+        if target > nums[-1]: return len(nums)
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target: return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+
+
+
+if __name__ == '__main__':
+    s = Solution()
+
+    print(s.searchInsert([1,3,5,7], 8))
+    print(s.searchInsert([1,3,5,7,9], 4))
